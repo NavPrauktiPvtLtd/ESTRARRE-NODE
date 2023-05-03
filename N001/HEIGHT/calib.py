d5898a57d94405b3640b7bdc9677b7ee323f224e
pran9957 <pbaishya604@gmai.com>
2023-05-03 10:54:12 +0100

    feat: height and tempetature

---
+++ N001/HEIGHT/calib.py
@@ -0,0 +1,29 @@
+# • For fast and high-accuracy long ranging (>2000 mm):
+#      – Total air gap and cover window thickness = 1 mm maximum
+#      – Cover window thickness <0.6 mm and air gap <0.4 mm
+#      – Gasket and/or light blocker enhances performance (see Section 5.2.7 Use of a gasket)
+# • For sub 1000 mm ranging:
+#      – Total air gap and cover window thickness = 2 mm maximum
+#      – Cover window thickness <1.5 mm and air gap <0.5 mm
+#      – Gasket is required
+
+
+
+import HEIGHT.driver.VL53L1X as VL53L1X
+
+tof = VL53L1X.VL53L1X(i2c_bus=1, i2c_address=0x29)
+tof.open()
+
+#tof.start_ranging(1)  # Start ranging
+                      # 0 = Unchanged
+                      # 1 = Short Range
+                      # 2 = Medium Range
+                      # 3 = Long Range
+
+
+
+#distance=input("Enter the standard distance", )
+distance=150
+tof.rspad_calibration
+value = tof.offset_callibration(distance)
+print(value)
---
+++ N001/TEMPERATURE/main.py
@@ -0,0 +1,55 @@
+from smbus2 import SMBus
+from time import sleep
+
+
+class MLX90614():
+    #ambiant temperature
+    MLX90614_TA = 0x06
+    # Object 1 temperature
+    MLX90614_TOBJ1 = 0x07
+
+    
+
+    comm_retries = 5
+    comm_sleep_amount = 0.1
+
+    def __init__(self, bus, address=0x5A):
+        self.bus = bus
+        self.address = address
+
+    def read_reg(self, reg_addr):
+        err = None
+        for i in range(self.comm_retries):
+            try:
+                return self.bus.read_word_data(self.address, reg_addr)
+            except IOError as e:
+                err = e
+                # "Rate limiting" - sleeping to prevent problems with sensor
+                # when requesting data too quickly
+                sleep(self.comm_sleep_amount)
+        # By this time, we made a couple requests and the sensor didn't respond
+        # (judging by the fact we haven't returned from this function yet)
+        # So let's just re-raise the last IOError we got
+        raise err
+
+    def read_temp(self, reg):
+        data = self.read_reg(reg)
+        temp = (data * 0.02) - 273.15
+        return temp
+
+    def get_amb_temp(self):
+        return self.read_temp(self.MLX90614_TA)
+
+    def get_obj_temp(self):
+        return self.read_temp(self.MLX90614_TOBJ1)
+
+
+
+channel = 1
+bus = SMBus(channel)
+sensor = MLX90614(bus, address=0x5A)
+amb_temp=round(sensor.get_amb_temp(), 2)
+obj_temp=round(sensor.get_obj_temp(), 2)
+print ('Ambiant temperature: '+ str(amb_temp)+'˚C')
+print ('Object temperature: '+ str(obj_temp)+'˚C')
+bus.close()
