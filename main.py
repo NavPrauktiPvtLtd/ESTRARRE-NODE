68716a978146aa913fc925aab15dee6ff4dcbc08
Bishal Roy <bishalroy895@gmail.com>
2023-06-21 11:46:20 +0530

    code refactor

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 from typing import Union
-
-
 from fastapi import FastAPI, HTTPException
+import logging
+from N001.TEMPERATURE.main import get_temp_data
+from N001.HEIGHT.main import get_height_data
+from N001.WEIGHT.main import get_weight_data
+from N001.WEIGHT.calib import offset_calc, ratio_calc, set_calib_value
+
 app = FastAPI()
 
-#logging file initialisation
-import logging
+# Logging file initialisation
 logging.basicConfig(filename="event.log",level=logging.DEBUG, format='%(asctime)s-%(levelname)s- %(message)s',
                      datefmt='%Y-%m-%d %H:%M:%S')
 
-#sensor dictionary
+# Sensor dictionary
 sensor = {'2':"HEIGHT",
           '5':"TEMPERATURE",
           '6':'WEIGHT'}
@@ -17,21 +20,15 @@ sensor = {'2':"HEIGHT",
 @app.get("/")
 async def read_root():
     data = await set_calib_value()
-    #print("hello_world")
-    return data #{"Hellos": "World"}
+    return data 
 
-# data request from sensors
-from N001.TEMPERATURE.main import get_temp_data
-from N001.HEIGHT.main import get_height_data
-from N001.WEIGHT.main import get_weight_data
 
+# Data request from sensors
 @app.get("/sensor/{sensor_id}/start")
 async def read_item(sensor_id: str, q: Union[str, None] = None):
     try:
         if sensor_id == '2':
             data = await get_height_data()
-        # if sensor_id == '4':
-        #     data = 1
         if sensor_id == '5':
             data = get_temp_data()
         if sensor_id == '6':
@@ -41,9 +38,8 @@ async def read_item(sensor_id: str, q: Union[str, None] = None):
     except:
         return 'Invalid Sensor ID'
 
-# # calibration of sensors
 
-from N001.WEIGHT.calib import offset_calc, ratio_calc, set_calib_value
+# Calibration of sensors
 @app.get("/sensor/weight/calib/{step}")
 async def read_item(step: str,offset: Union[int, None] = None, ratio: Union[float, None] = None,):
     try:
