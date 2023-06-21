759b893d23acbec49b70c6bf20ef4d7832b8e268
Bishal Roy <bishalroy895@gmail.com>
2023-06-21 13:05:18 +0530

    feat: add loggers to the requests

--- main.py
+++ main.py
@@ -9,8 +9,7 @@ from N001.WEIGHT.calib import offset_calc, ratio_calc, set_calib_value
 app = FastAPI()
 
 # Logging file initialisation
-logging.basicConfig(filename="event.log",level=logging.DEBUG, format='%(asctime)s-%(levelname)s- %(message)s',
-                     datefmt='%Y-%m-%d %H:%M:%S')
+logging.basicConfig(filename="event.log",level=logging.DEBUG, format='%(asctime)s-%(levelname)s- %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
 # Sensor dictionary
 sensor = {'2':"HEIGHT",
@@ -19,13 +18,23 @@ sensor = {'2':"HEIGHT",
 
 @app.get("/")
 async def read_root():
+    
+    # Logging the GET request
+    logging.info("GET /")
+    
     data = await set_calib_value()
+    
+    # Logging the data
+    logging.info(data)
     return data 
 
 
 # Data request from sensors
 @app.get("/sensor/{sensor_id}/start")
 async def read_item(sensor_id: str, q: Union[str, None] = None):
+    
+    # logging the GET request
+    logging.info("GET /sensor/{sensor_id}/start")
     try:
         if sensor_id == '2':
             data = await get_height_data()
@@ -33,15 +42,25 @@ async def read_item(sensor_id: str, q: Union[str, None] = None):
             data = get_temp_data()
         if sensor_id == '6':
             data = get_weight_data()
+            
+        logging.INFO(data)
 
         return {"sensor_id": sensor_id,"sensor_name":sensor[sensor_id], 'data': data}
     except:
-        return 'Invalid Sensor ID'
+        
+        # Logging the error
+        logging.error("Invalid Sensor ID")
+        
+        return {"error": "Invalid Sensor ID"}
 
 
 # Calibration of sensors
 @app.get("/sensor/weight/calib/{step}")
-async def read_item(step: str,offset: Union[int, None] = None, ratio: Union[float, None] = None,):
+async def read_item(step: str,offset: Union[int, None] = None, ratio: Union[float, None] = None):
+    
+    # Logging the GET request
+    logging.info("GET /sensor/weight/calib/{step}")
+    
     try:
         if step == '1':
             data = await offset_calc()
@@ -51,7 +70,11 @@ async def read_item(step: str,offset: Union[int, None] = None, ratio: Union[floa
             return {"data_type":'ratio', 'data': data}
         if step == '3':
             set_calib_value(offset, ratio)
-            return 'data_recieved'
+            return {"success": "data_recieved"}
+    
     except Exception as e:
-        return 'invalid step'
+        
+        # Logging the error
+        logging.error(e)
+        return {"error": "invalid step", "data": str(e)}
     
